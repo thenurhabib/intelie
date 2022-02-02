22c93a0ff514cbca5a8fc5c78b54645bec1b59c5
Md. Nur habib <68025241+thenurhabib@users.noreply.github.com>
2022-02-02 23:28:25 +0600

    Add files via upload
    Version Two.

---
+++ header.py
@@ -0,0 +1,4 @@
+userAgent = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+}
+cookies = {"fr": "0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy"}
